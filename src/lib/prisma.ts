49ca311f5675257256f5bae8b16e0899fff7a35b
KARTHIKEYAN MP <karthikeymp@gmail.com>
2026-07-29 23:40:52 +0530

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,7 @@
-import { PrismaClient } from '@/generated/prisma/client';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+import { PrismaClient } from '@prisma/client';
 
-function createPrismaClient() {
-  const adapter = new PrismaBetterSqlite3({ url: 'dev.db' });
-  return new PrismaClient({ adapter });
-}
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
-export const prisma: PrismaClient =
-  globalForPrisma.prisma ?? (globalForPrisma.prisma = createPrismaClient());
-
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
